3a9f37a6fd63cd193fef0029038bcb33bbe23cc7
Vivaan Baid <baidvivaan782@gmail.com>
2022-02-17 23:36:22 -0500

    Initial Commit

---
+++ FlagRest/FlagRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagRestApp.swift
+//  FlagRest
+//
+//  Created by Vivaan Baid on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
